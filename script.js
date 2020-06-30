0a5e33946259372b4f9e6f92799713619ec3d7f4
SaraCremonesi <saracrem00@gmail.com>
2020-06-30 17:09:49 +0200

    aggiornamenti

--- script.js
+++ script.js
@@ -22,41 +22,41 @@ function trovaMese(data) {
       month: data.month(),
       year: data.year()
     });
-  };
+    var context = {
+      date: giornoCorrente.format('D MMMM')
+    };
+    var html = template(context);
 
-  var context = {
-    date: giornoCorrente.format('D MMMM')
+    $('#lista-giorni').append(html);
   };
-  var html = template(context);
-
-  $('#lista-giorni').append(html);
 };
 });
 
-// function trovaFestivita(data) {
-//   $.ajax(
-//     {
-//       url: 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0',
-//       method: 'GET',
-//       data: {
-//         year: data.year(),
-//         month: data.month()
-//       },
-//       success: function(dataResponse) {
-//       var festivita = dataResponse.response;
-//       for (var i = 0; i < festivita; i++) {
-//         var festivitaCorrente = festivita[i];
-//         $('li').each(function() {
-//           var questoGiorno = $(this).attr('data-day');
-//           if (questoGiorno === festivitaCorrente.date) {
-//             $(this).addClass('red');
-//             $(this).append('(' + festivitaCorrente.name + ')')
-//           }
-//         })
-//       }
-//     },
-//     error: function() {
-//       alert('Errore');
-//     }
-// });
-// };
+function trovaFestivita(data) {
+  $.ajax(
+    {
+      url: 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0',
+      method: 'GET',
+      data: {
+        year: data.year(),
+        month: data.month()
+      },
+      success: function(dataResponse) {
+      var festivita = dataResponse.response;
+      for (var i = 0; i < festivita.length; i++) {
+        var festivitaCorrente = festivita[i];
+
+        $('.day').each(function() {
+          var questoGiorno = $(this).attr('data-day');
+          if (questoGiorno === festivitaCorrente.date) {
+            $(this).addClass('.red');
+            $(this).append('(' + festivitaCorrente.name + ')')
+          }
+        })
+      }
+    },
+    error: function() {
+      alert('Errore');
+    }
+});
+};
