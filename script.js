9f4cd7f3e98a2cbed261326e84969d48ab0325c1
SaraCremonesi <saracrem00@gmail.com>
2020-06-30 16:13:26 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+  var data = moment({
+    day: 1,
+    month: 0,
+    year: 2018
+  });
+
+  trovaMese(data);
+  trovaFestivita(data);
+
+
+// *************FUNZIONI***************
+function trovaMese(data) {
+  
+  var numeroGiorni = data.daysInMonth();
+
+  var source = $('#calendario').html();
+  var template = Handlebars.compile(source);
+  for (var i = 0; i < numeroGiorni; i++) {
+    var giornoCorrente = moment({
+      day: i,
+      month: data.month(),
+      year: data.year()
+    });
+  };
+
+  var context = {
+    date: giornoCorrente.format('YYYY-MM-DD')
+  };
+  var html = template(context);
+
+  $('#lista-giorni').append(html);
+};
+});
+
+function trovaFestivita(data) {
+  $.ajax(
+    {
+      url: 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0',
+      method: 'GET',
+      data: {
+        year: data.year(),
+        month: data.month()
+      },
+      success: function(dataResponse) {
+      var festivita = dataResponse.response;
+      for (var i = 0; i < festivita; i++) {
+        var festivitaCorrente = festivita[i];
+        $('li').each(function() {
+          var questoGiorno = $(this).attr('data-day');
+          if (questoGiorno === festivitaCorrente.date) {
+            $(this).addClass('red');
+            $(this).append('(' + festivitaCorrente.name + ')')
+          }
+        })
+      }
+    },
+    error: function() {
+      alert('Errore');
+    }
+});
+};
