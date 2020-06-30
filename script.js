fa786bd6bbbb9d6d7c522d20f1250946e181ba08
SaraCremonesi <saracrem00@gmail.com>
2020-06-30 16:50:36 +0200

    aggiornamento

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ $(document).ready(function() {
 
 // *************FUNZIONI***************
 function trovaMese(data) {
-  
+  $('#mese').text(data.format('MMMM YYYY'));
   var numeroGiorni = data.daysInMonth();
 
   var source = $('#calendario').html();
   var template = Handlebars.compile(source);
-  for (var i = 0; i < numeroGiorni; i++) {
+  for (var i = 1; i <= numeroGiorni; i++) {
     var giornoCorrente = moment({
       day: i,
       month: data.month(),
@@ -25,7 +25,7 @@ function trovaMese(data) {
   };
 
   var context = {
-    date: giornoCorrente.format('YYYY-MM-DD')
+    date: giornoCorrente.format('D MMMM')
   };
   var html = template(context);
 
@@ -33,30 +33,30 @@ function trovaMese(data) {
 };
 });
 
-function trovaFestivita(data) {
-  $.ajax(
-    {
-      url: 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0',
-      method: 'GET',
-      data: {
-        year: data.year(),
-        month: data.month()
-      },
-      success: function(dataResponse) {
-      var festivita = dataResponse.response;
-      for (var i = 0; i < festivita; i++) {
-        var festivitaCorrente = festivita[i];
-        $('li').each(function() {
-          var questoGiorno = $(this).attr('data-day');
-          if (questoGiorno === festivitaCorrente.date) {
-            $(this).addClass('red');
-            $(this).append('(' + festivitaCorrente.name + ')')
-          }
-        })
-      }
-    },
-    error: function() {
-      alert('Errore');
-    }
-});
-};
+// function trovaFestivita(data) {
+//   $.ajax(
+//     {
+//       url: 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0',
+//       method: 'GET',
+//       data: {
+//         year: data.year(),
+//         month: data.month()
+//       },
+//       success: function(dataResponse) {
+//       var festivita = dataResponse.response;
+//       for (var i = 0; i < festivita; i++) {
+//         var festivitaCorrente = festivita[i];
+//         $('li').each(function() {
+//           var questoGiorno = $(this).attr('data-day');
+//           if (questoGiorno === festivitaCorrente.date) {
+//             $(this).addClass('red');
+//             $(this).append('(' + festivitaCorrente.name + ')')
+//           }
+//         })
+//       }
+//     },
+//     error: function() {
+//       alert('Errore');
+//     }
+// });
+// };
